a33d3e29e97831d47850313087893247cb655861
Ali Karimi <alikarimi.s1376@gmail.com>
2025-04-21 12:30:55 +0330

    added socks proxy to forward dns https request over socks proxy

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
-import dgram, { RemoteInfo } from 'node:dgram';
-import dotenv from 'dotenv';
 import packet, { Answer, Packet } from 'dns-packet';
+import dotenv from 'dotenv';
+import https from 'https';
+import dgram, { RemoteInfo } from 'node:dgram';
+import { SocksProxyAgent } from 'socks-proxy-agent';
 import config from './../config/config.json';
 
 dotenv.config()
 
 const PORT = process.env.PORT ?? 53 // DNS Server default port is 53
 const customRecords: Record<string, string> = config.customRecords
+
+// Config socks proxy
+const socksProxy = process.env.SOCKS_PROXY
+if (!socksProxy) throw new Error('Socks url is required')
+const agent = new SocksProxyAgent(socksProxy)
+
+// Config DNS over HTTPS server
+const dnsServerHostname = process.env.DNS_SERVER_HOSTNAME ?? "cloudflare-dns.com"
+const dnsServerApiPath = process.env.DNS_SERVER_API_PATH ?? "/dns-query"
+
 
 // Create UDP server
 const server = dgram.createSocket('udp4')
@@ -36,12 +48,63 @@ server.on('message', (queryMessage: Buffer, rinfo: RemoteInfo): void => {
       if (err) {
         console.error('Error sending DNS response:', err);
       } else {
-        console.log(`Sent response to ${rinfo.address}:${rinfo.port}`)
+        console.info(`Sent response to ${rinfo.address}:${rinfo.port}`)
       }
     })
 
     return
   }
+
+
+  // Step3: Send dns query over https
+  const options = {
+    hostname: dnsServerHostname,
+    port: 443,
+    path: dnsServerApiPath,
+    method: 'POST',
+    agent: agent,
+    headers: {
+      'Content-Type': 'application/dns-message',
+      'Content-Length': queryMessage.length
+    }
+  }
+
+  // Send the DoH query through the SOCKS proxy
+  const req = https.request(options, (res) => {
+    const chunks: any[] = [];
+
+    res.on('data', (chunk) => {
+      chunks.push(chunk);
+    });
+
+    res.on('end', () => {
+      const queryResponse = Buffer.concat(chunks);
+
+      // Step 4: answer query request back
+      try {
+        // Decode DNS response to check doesn't have error, possible not necessary!
+        const decoded = packet.decode(queryResponse);
+
+        server.send(queryResponse, 0, queryResponse.length, rinfo.port, rinfo.address, (err: Error | null) => {
+          if (err) {
+            console.error('\x1b[31mError sending DNS response:\x1b[0m', err)
+          } else {
+            console.info(`\x1b[32mSent Response\x1b[0m to \x1b[33m${rinfo.address}:${rinfo.port}\x1b[0m`)
+          }
+        })
+      } catch (err) {
+        console.error('Failed to decode DNS response:', err)
+      }
+    })
+  })
+
+  req.on('error', (err: Error) => {
+    console.error('\x1b[31mSocks request error:\x1b[0m', err.message)
+  })
+
+  // Send the query buffer
+  req.write(queryMessage)
+  req.end()
 
 })
 
